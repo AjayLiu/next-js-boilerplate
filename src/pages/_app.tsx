bdba0320b6441f210aaf321a6c94390163131eee
AjayLiu <ajayliu2928@gmail.com>
2021-02-12 13:54:52 -0800

    update boilerplate with functioning project

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,6 @@
 import type {AppProps} from 'next/app'
 
 import '@styles/globals.css'
-import '@styles/faq.css'
 function Application({ Component, pageProps } : AppProps) {
   return <Component {...pageProps} />
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,5 @@
 import Head from 'next/head'
-// import Header from '@components/Header'
 import Footer from '@components/Footer'
-import LandingPage from '@components/LandingPage'
-import About from '@components/About'
-import Projects from '@components/Projects'
-import Skills from '@components/Skills'
-import Contact from '@components/Contact'
-import FAQ from '@components/FAQ'
 import GoogleAnalyticsHook from '@components/GoogleAnalyticsHook'
 
 const Home: React.FC = () => {
@@ -29,16 +22,10 @@ const Home: React.FC = () => {
         <meta property="og:image" content="https://ajayliu.com/imgs/ajayliudark.png" />
         <meta property="og:description" content="My name is Ajay Liu and I love creating things!" />
 
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/three.js/r121/three.min.js"></script>
       </Head>
 
       <main>
-        <LandingPage />
-        <About />
-        <Projects />
-        <Skills />
-        <Contact email="contact@ajayliu.com"/>
-        <FAQ />
+       
       </main>
 
       <Footer />
